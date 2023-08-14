b9ce8ccff6166eb6465fa51e7938397a2d1d8d1f
Nicolas Marra <68780542+nicolasmarra@users.noreply.github.com>
2023-08-14 12:05:10 +0200

    update menu nav

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const my_header = document.getElementById("my_header");
-window.addEventListener( "scroll", () => 
+
+window.addEventListener( "scroll", headerScroll);
+
+
+function headerScroll ()
 {
+
 
     const scrollY = window.scrollY;
     if(scrollY != 0)
@@ -8,6 +13,25 @@ window.addEventListener( "scroll", () =>
         my_header.classList.add('scrolled')
     else
         my_header.classList.remove('scrolled')
-});
+}
 
+function openMenu()
+{
+    const my_button = document.getElementById("open-menu");
+    const my_menu = document.getElementById("my-menu")
+    const my_page = document.getElementById("page")
+    if(!my_button.classList.contains("closed"))
+    {
+        my_button.classList.add("closed")
+        my_menu.classList.add('hidden')
+        my_header.classList.remove('scrolled')
+        my_page.classList.remove("hidden")   
+    }
+    else{
 
+        my_button.classList.remove("closed")
+        my_menu.classList.remove('hidden')
+        my_header.classList.add('scrolled')
+        my_page.classList.add("hidden")
+    }
+}
